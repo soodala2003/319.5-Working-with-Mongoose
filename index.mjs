a05786034b4380da2b7472460a50c7b1d38270c6
soodala2003 <soodala78@gmail.com>
2024-11-12 11:18:14 -0500

    updated index.mjs

--- index.mjs
+++ index.mjs
@@ -15,45 +15,47 @@ app.use(express.json());
 await mongoose.connect("mongodb+srv://soodala78:1972And78@mongopractice.g7ss4.mongodb.net/sample_training");
 
 const newDoc = new Grade({
-    learner_id: 2,
-    class_id: 213,
-    scores: [{ type: "exam", score: 50 },
-             { type: "quiz", score: 60 },
-             { type: "homework", score: 70 },
-             { type: "homework", score: 80 },
-    ],
+  learner_id: 2,
+  class_id: 213,
+  scores: [{ type: "exam", score: 50 },
+           { type: "quiz", score: 60 },
+           { type: "homework", score: 70 },
+           { type: "homework", score: 80 },
+  ],
 });
 
 async () => {
-    await newDoc.save();
+  await newDoc.save();
 };
 
 app.get("/", (req, res) => {
-    res.send("ALAB 319.5 - Working with Mongoose.");
+  res.send("ALAB 319.5 - Working with Mongoose.");
 });
-
-//app.use("/grades", grades);
-//app.use("/grades", grades_agg);
 
 // Get a single grade entry
 app.get("/grades/:id", async (req, res) => {
-    try {
-        let result = await Grade.findById(req.params.id);
-        res.send(result);
-    } catch {
-        res.send("Invalid ID").status(400);
-    }
+  try {
+    let result = await Grade.findById(req.params.id);
+    res.send(result);
+  } catch {
+    res.send("Invalid ID").status(400);
+  }
 });
 
-/* router.get("/:id", async (req, res) => {
+// Create a single grade entry
+app.post("/", async (req, res) => {
   let collection = await db.collection("grades");
-  let query = { _id: ObjectId(req.params.id) };
-  let result = await collection.findOne(query);
+  let newDocument = req.body;
 
-  if (!result) res.send("Not found").status(404);
-  else res.send(result).status(200);
-}); */
+  // rename fields for backwards compatibility
+  if (newDocument.student_id) {
+    newDocument.learner_id = newDocument.student_id;
+    delete newDocument.student_id;
+  }
 
+  let result = await collection.insertOne(newDocument);
+  res.send(result).status(204);
+}); 
 
 // Global error handling
 app.use((err, _req, res, next) => {
